f3abd9efbb276f51e579b8b175b71f931246663c
kuga <esixterb58@gmail.com>
2025-05-11 21:28:14 +0300

    feat: day-1

--- index.js
+++ index.js
@@ -1,1 +1,592 @@
 'use strict'
+
+// 1
+// 1.1
+for (let i = 1; i <= 5; i++) {
+  console.log("Цикл for, шаг: ", i)
+}
+
+for (let z = 5; z >= 0; z--) {
+  console.log("z =", z)
+}
+
+for (let g = 0; g < 3; g++) {
+  console.log("g =", g)
+}
+
+for (let k = 10; k >= 5; k--) {
+  console.log("k =", k)
+}
+
+let r
+for (r = 3; r < 5; r++) {
+  console.log("r =", r)
+}
+
+for (let f1 = 8; f1 > 7; f1--) {
+  console.log("f1 =", f1)
+}
+
+for (let f2 = 4; f2 < 7; f2++) {
+  console.log("f2 =", f2)
+}
+
+for (let f3 = 1; f3 <= 3; f3++) {
+  console.log("f3 =", f3)
+}
+
+for (let f4 = 111; f4 >= 101; f4--) {
+  console.log("f4 =", f4)
+}
+
+for (let f5 = 0; f5 < 4; f5++) {
+  console.log("f5 =", f5)
+}
+
+for (let f6 = 10000; f6 >= 9999; f6--) {
+  console.log("f6 =", f6)
+}
+
+for (let f7 = 34; f7 <= 46; f7++) {
+  console.log("f7 =", f7)
+}
+
+for (let f8 = 28; f8 > 20; f8--) {
+  console.log("f8 =", f8)
+}
+
+for (let f9 = 14; f9 < 19; f9++) {
+  console.log("f9 =", f9)
+}
+
+for (let q1 = 99; q1 >= 90; q1--) {
+  console.log("q1 =", q1)
+}
+
+for (let q2 = 1; q2 <= 10; q2++) {
+  console.log("q2 =", q2)
+}
+
+for (let q3 = 10; q3 < 21; q3++) {
+  console.log("q3 =", q3)
+}
+
+for (let q4 = 24; q4 > 12; q4--) {
+  console.log("q4 =", q4)
+}
+
+for (let q5 = 999; q5 >= 888; q5--) {
+  console.log("q5 =", q5)
+}
+
+for (let q6 = 123; q6 <= 321; q6++) {
+  console.log("q6 =", q6)
+}
+
+// 1.2
+let j = 0
+while (j < 6) {
+  j++
+  console.log(j)
+}
+
+let j1 = 30
+while (j1 > 10) {
+  j1--
+  console.log("j1 =", j1)
+}
+
+let j2 = 5
+while (j2 < j1) {
+  j2++
+  console.log("j2 =", j2)
+}
+
+let j3 = 17
+while (j3 <= 20) {
+  console.log("j3 =", j3)
+  j3++
+}
+
+let j4 = 1
+while (j4 < 5) {
+  console.log("j4 =", j4)
+  j4++
+}
+
+let j5 = 5
+while (j5 < 10) {
+  console.log("j5 =", j5)
+  j5++
+}
+
+let j6 = 6
+while (j6 > 3) {
+  console.log("j6 =", j6)
+  j6--
+}
+
+let j7 = 7
+while (j7 <= 10) {
+  console.log("j7 =", j7)
+  j7++
+}
+
+let j8 = 8
+while (j8 >= 4) {
+  console.log("j8 =", j8)
+  j8--
+}
+
+let j9 = 9
+while (j9 < 11) {
+  console.log("j9 =", j9)
+  j9++
+}
+
+let e = 0
+while (e < 1) {
+  console.log("e =", e)
+  e++
+}
+
+let e1 = 1
+while (e1 < 2) {
+  console.log("e1 =", e1)
+  e1++
+}
+
+let e2 = 2
+while (e2 < 3) {
+  console.log("e2 =", e2)
+  e2++
+}
+
+let e3 = 3
+while (e3 < 4) {
+  console.log("e3 =", e3)
+  e3++
+}
+
+let e4 = 4
+while (e4 < 5) {
+  console.log("e4 =", e4)
+  e4++
+}
+
+let e5 = 5
+while (e5 < 6) {
+  console.log("e5 =", e5)
+  e5++
+}
+
+let e6 = 6
+while (e6 < 7) {
+  console.log("e6 =", e6)
+  e6++
+}
+
+let e7 = 7
+while (e7 < 8) {
+  console.log("e7 =", e7)
+  e7++
+}
+
+let e8 = 8
+while (e8 < 9) {
+  console.log("e8 =", e8)
+  e8++
+}
+
+let e9 = 9
+while (e9 < 10) {
+  console.log("e9 =", e9)
+  e9++
+}
+ 
+// 1.3
+let n = 0
+do {
+  console.log("n =", n)
+  n++
+} while (n < 1)
+
+let n1 = 1
+do {
+  console.log("n1 =", n1)
+  n1++
+} while (n1 < 2)
+
+let n2 = 2
+do { 
+  console.log("n2 =", n2)
+  n2++
+} while (n2 < 3)
+
+let n3 = 3
+do {
+  console.log("n3 =", n3)
+  n3++
+} while (n3 < 4)
+
+let n4 = 4
+do {
+  console.log("n4 =", n4)
+  n4++
+} while (n4 < 5)
+
+let n5 = 5
+do {
+  console.log("n5 =", n5)
+  n5++
+} while (n5 < 6)
+
+let n6 = 6
+do {
+  console.log("n6 =", n6)
+  n6++
+} while (n6 < 7)
+
+let n7 = 7
+do {
+  console.log("n7 =", n7)
+  n7++
+} while (n7 < 8)
+
+let n8 = 8
+do {
+  console.log("n8 =", n8)
+  n8++
+} while (n8 < 9)
+
+let n9 = 9
+do {
+  console.log("n9 =", n9)
+  n9++
+} while (n9 < 10)
+
+let t = 9
+do {
+  console.log("t =", t)
+  t--
+} while (t > 8)
+
+let t1 = 8
+do {
+  console.log("t1 =", t1)
+  t1--
+} while (t1 > 7)
+
+let t2 = 7
+do {
+  console.log("t2 =", t2)
+  t2--
+} while (t2 > 6)
+
+let t3 = 6
+do { 
+  console.log("t3 =", t3)
+  t3--
+} while (t3 > 5)
+
+let t4 = 5
+do {
+  console.log("t4 =", t4)
+  t4--
+} while (t4 > 4)
+
+let t5 = 4
+do {
+  console.log("t5 =", t5)
+  t5--
+} while (t5 > 3)
+
+let t6 = 3
+do {
+  console.log("t6 =", t6)
+  t6--
+} while (t6 > 2)
+
+let t7 = 2
+do {
+  console.log("t7 =", t7)
+  t7--
+} while (t7 > 1)
+
+let t8 = 1
+do {
+  console.log("t8 =", t8)
+  t8--
+} while (t8 > 0)
+
+let t9 = 0
+do {
+  console.log("t9 =", t9)
+  t9--
+} while (t9 > 0)
+
+// 1.4
+const obj = {a: 1, b: 22, c: 333, d: 4444, e: 55555} //перберает ключи
+for (const el in obj) {
+  console.log(el)
+}
+
+const obj1 = {a: 1, b: 2, c: 3}
+for (const el in obj1) {
+  console.log(el)
+}
+
+const obj2 = {name: 'vlad', age: 23, isStudent: true}
+for (const el in obj2) {
+  console.log(el)
+}
+
+const obj3 = {top: 100, right: 80, bottom: 50, left: 30}
+for (const el in obj3) {
+  console.log(el)
+}
+
+const obj4 = {x: 5, y: 12, z: 4}
+for (const el in obj4) {
+  console.log(el)
+}
+
+const obj5 = {step1: "Шаг 1", step2: "Шаг 2", step3: "Шаг 3"}
+for (const el in obj5) {
+  console.log(el)
+}
+
+const obj6 = {st3: 3, st2: 2, st1: 1}
+for (const el in obj6) {
+  console.log(el)
+}
+
+const obj7 = {d: "1", e: "2", f: "3"}
+for (const el in obj7) {
+  console.log(el)
+}
+
+const obj8 = {a: 30, b: 40}
+for (const el in obj8) {
+  console.log(el)
+}
+
+const obj9 = {1: "a", 2: "b"}
+for (const el in obj9) {
+  console.log(el)
+}
+
+const ob = {num1: 1, num2: 2, num3: 3}
+for (const el in ob) {
+  console.log(el)
+}
+
+const ob1 = {a: 1}
+for (const el in ob1) {
+  console.log(el)
+}
+
+const ob2 = {b: 2}
+for (const el in ob2) {
+  console.log(el)
+}
+
+const ob3 = {c: 3}
+for (const el in ob3) {
+  console.log(el)
+}
+
+const ob4 = {d: 4}
+for (const el in ob4) {
+  console.log(el)
+}
+
+const ob5 = {e: 5}
+for (const el in ob5) {
+  console.log(el)
+}
+
+const ob6 = {f: 6}
+for (const el in ob6) {
+  console.log(el)
+}
+
+const ob7 = {g: 7}
+for (const el in ob7) {
+  console.log(el)
+}
+
+const ob8 = {h: 8}
+for (const el in ob8) {
+  console.log(el)
+}
+
+const ob9 = {i: 9}
+for (const el in ob9) {
+  console.log(el)
+}
+
+// 1.5
+const arr = [1, 2, 3]
+for (const el of arr) {
+  console.log(el)
+}
+
+const arr1 = ["a", "b", "c"]
+for (const el of arr1) {
+  console.log(el)
+}
+
+const arr2 = [3, 2, 1]
+for (const el of arr2) {
+  console.log(el)
+}
+
+const arr3 = [4, 5, 6]
+for (const el of arr3) {
+  console.log(el)
+}
+
+const arr4 = ["d", "e", "f"]
+for (const el of arr4) {
+  console.log(el)
+}
+
+const arr5 = ["g", "h", "i"]
+for (const el of arr5) {
+  console.log(el)
+}
+
+const arr6 = [7, 8, 9]
+for (const el of arr6) {
+  console.log(el)
+}
+
+const arr7 = [10, 11, 12]
+for (const el of arr7) {
+  console.log(el)
+}
+
+const arr8 = ["hi", "ola", "nihao"]
+for (const el of arr8) {
+  console.log(el)
+}
+
+const arr9 = [1, 11, 111]
+for (const el of arr9) {
+  console.log(el)
+}
+
+const ar = [123, 231, 312]
+for (const el of ar) {
+  console.log(el)
+}
+
+const ar1 = ["1", "2", "3"]
+for (const el of ar1) {
+  console.log(el)
+}
+
+const ar2 = [9, 8, 7]
+for (const el of ar2) {
+  console.log(el)
+}
+
+const ar3 = [11, 22, 33]
+for (const el of ar3) {
+  console.log(el)
+}
+
+const ar4 = [1, 2, 3, 4, 5]
+for (const el of ar4) {
+  console.log(el)
+}
+
+const ar5 = ["unu", "doi", "trey"]
+for (const el of ar5) {
+  console.log(el)
+}
+
+const ar6 = [6, 5, 4]
+for (const el of ar6) {
+  console.log(el)
+}
+
+const ar7 = [4444, 55555, 666666]
+for (const el of ar7) {
+  console.log(el)
+}
+
+const ar8 = ["string-1", "string-2", "string-3"]
+for (const el of ar8) {
+  console.log(el)
+}
+
+const ar9 = ["yes", "no"]
+for (const el of ar9) {
+  console.log(el)
+}
+
+
+// 2
+// 2.1
+const array = [2, 3, 4]
+for (let i = 0; i < array.length; i++) {
+  console.log(array[i] * array[i])
+}
+
+// 2.2
+const array1 = ["string", "num", "liquiMoly"]
+let counter = 0
+while (counter < array1.length) {
+  const lengthString = array1[counter].length
+  console.log(lengthString)
+  counter++
+}
+
+// 2.3
+let userNum//объявил переменную (число которое вводит пользователь)
+do {
+  userNum = prompt("Введите число не больше 10")//вывожу сообщение
+  userNum = Number(userNum)//указываю то что будет вводить пользователь должно преобразоваться в числовой тип
+} while (userNum <= 10)
+
+//2.4
+const user = {
+  name: "Anna",
+  age: 30,
+  isStudent: true,
+}
+
+for (const key in user) {
+  console.log(key)
+}
+
+// 2.5
+const colors = ["red", "green", "blue"]
+
+for (const element of colors) {
+  console.log(element)
+}
+
+// 2.6
+const car = {
+  brand: 'BMW',
+  model: 'M3 E46',
+  year: 2002,
+}
+const carKey = []
+for (const key in car) {
+  carKey.push(key)
+}
+console.log(carKey)
+
+// 2.7
+const products = [{name: "Phone"}, {name: "Tablet"}]
+for (const element of products) {
+  console.log(element.name)// обращаемся к ключу чтобы он вывел своё содержимое 
+  // console.log(element) если написать так то выведет целый объект
+}
+
+// 4
+// 4.1
